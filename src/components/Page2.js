ca7886898b5ff2dfb6a93587f05832bf606b48ed
nwankpa-D-boy <nwankpakelechisamuel@gmail.com>
2023-12-10 01:20:54 +0000

    first commit

---
+++ src/components/Page2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Page2 = ({ onRouteChange }) => {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <button onClick={() => onRouteChange('page1')}>Page 1</button>
+            <button className='disabled'>Page2</button>
+            <button onClick={() => onRouteChange('page3')}>Page 3</button>
+        </div>
+    )
+}
+
+export default Page2;
